6aa64e70e44a68c469571ac284ddb065b31a881d
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-05-04 12:26:26 +0200

    adicionado rotas

--- src/app.js
+++ src/app.js
@@ -2,8 +2,22 @@ import express from "express";
 
 const app = express (); //conjunto de codigo do express dentro da var app
 
+const livros = [
+    {
+        id: 1,
+        titulo: "O senhor do anéis"
+    },
+    {
+        id: 2,
+        titulo: "O hobbit"
+    }
+]
 app.get("/", (req, res) => { //gerencia as rotas
     res.status(200).send("Curso de Node.Js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
 });
 
 export default app; //exporta o modulo
