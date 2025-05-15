aa94ade0f8920dcaec55133b149bf3ff98e953f5
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-05-15 20:26:19 +0200

    acessado a coleção livros no mongoDB

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
 
 const conexao = await conectaNaDatabase();
 
@@ -14,34 +15,18 @@ conexao.once("open", () => {
 const app = express (); 
 app.use(express.json()); 
 
-
-const livros = [
-    {
-        id: 1,
-        titulo: "O senhor do anéis"
-    },
-    {
-        id: 2,
-        titulo: "O hobbit"
-    }
-]
-
-function buscaLivros(id) {
-    return livros.findIndex(livros => {
-        return livros.id === Number(id);
-    })
-}
 app.get("/", (req, res) => { //gerencia as rotas
-    res.status(200).send("Curso de Node.Js");
+    res.status(200).send("Conectado");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});   
+    res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
     const index = buscaLivros(req.params.id);
-    res.status(200).json(livros[index]);
+    res.status(200).json(livro[index]);
 });
 
 app.post("/livros", (req, res) => {
@@ -62,5 +47,3 @@ app.delete("/livros/:id", (req, res) => {
 });
 
 export default app; //exporta o modulo
-
-//mongodb+srv://laizacatafesta:<db_password>@cluster0.32fsz4z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
  import mongoose, {mongo} from "mongoose";
 
  async function conectaNaDatabase() {
-    mongoose.connect("mongodb+srv://laizacatafesta:Alura123@cluster0.32fsz4z.mongodb.net/livraria/?retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
     return mongoose.connection;
     };
 
