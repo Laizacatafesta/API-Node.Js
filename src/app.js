d531d5a33076e61d992fa35bb4cc09e0d3586f4a
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-05-16 21:54:23 +0200

    add livro controller

--- src/app.js
+++ src/app.js
@@ -19,10 +19,10 @@ app.get("/", (req, res) => { //gerencia as rotas
     res.status(200).send("Conectado");
 });
 
-app.get("/livros", async (req, res) => {
-    const listaLivros = await livro.find({});   
-    res.status(200).json(listaLivros);
-});
+// app.get("/livros", async (req, res) => {
+//     const listaLivros = await livro.find({});   
+//     res.status(200).json(listaLivros);
+// });
 
 app.get("/livros/:id", (req, res) => {
     const index = buscaLivros(req.params.id);
---
+++ src/controllers/livrosControllers.js
@@ -0,0 +1,17 @@
+//centralizar todas as logicas que esta relacionada as acções que podem ser feitas em um livro.
+//o que as rotas vão chamar para executar as operações e o manuseio das requisições e respostas correspondentes.
+
+//Controllers contêm a lógica que responde às requisições HTTP.
+
+import livro from  "../models/Livro.js";
+
+class LivroController {
+
+    static async listarLivros(req, res) {
+        const listaLivros = await livro.find({}); //Está buscando todos os documentos na coleção livros e armazenando na var. as chaves {} indica que não há filtro, ou seja, retorna todos.
+        //Essa linha de cima assume que você já tem um modelo Mongoose chamado livro importado em algum lugar (fora do trecho que você colou).
+        res.status(200).json(listaLivros);
+    };
+};
+
+export default LivroController;
