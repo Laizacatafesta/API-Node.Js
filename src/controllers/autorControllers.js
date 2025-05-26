99c490d8956be3a445226751f5ace264cbde7b14
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-05-26 17:46:49 +0200

    corrigido cadastro e adicionado update

--- src/controllers/autorControllers.js
+++ src/controllers/autorControllers.js
@@ -22,15 +22,22 @@ class AutorController {
 
     static async cadastrarAutorById(req, res) {
         try {
-            const id = req.params.id;
-            const cadastrarAutor = await autor.create(req.body);
+            const novoAutor = await autor.create(req.body);
             res.status(200).json({message: "Autor cadastrado com sucesso!", autor: novoAutor});
         } catch (erro) {
             res.status(500).json({message: `${erro.message} - Falha na requisição`});
         };
     };
 
-    
+   static async atualizarAutor(req, res) {
+        try {
+            const id = req.params.id;
+            const atualizarAutor = await autor.findByIdAndUpdate(id, req.body);
+            res.status(200).json({"Autor atualizado com sucesso"});
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha na atualização`});
+        };
+   };
 };
 
 export default AutorController;
